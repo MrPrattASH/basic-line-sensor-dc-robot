0308202ac553b35492908206caa59a36b68fa3e9
Brogan Pratt <101632496+MrPrattASH@users.noreply.github.com>
2022-10-17 14:08:37 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ light_sensor = 0
 def on_forever():
     global light_sensor
     light_sensor = pins.analog_read_pin(AnalogPin.P1)
-    if light_sensor >= 700:
+    if light_sensor >= 850:
         pins.digital_write_pin(DigitalPin.P16, 1)
+        #back up
+        motor.motor_run(motor.Motors.M1, motor.Dir.CCW, 75)
+        motor.motor_run(motor.Motors.M2, motor.Dir.CCW, 85)
+        basic.pause(1500)
+        #spin around at a random degree
+        motor.motor_run(motor.Motors.M1, motor.Dir.CW, 75)
+        motor.motor_run(motor.Motors.M2, motor.Dir.CCW, 75)
+        basic.pause(randint(750,2000))
+
     else:
+        #we're on white, drive forward
         pins.digital_write_pin(DigitalPin.P16, 0)
-Math.random()
+        motor.motor_run(motor.Motors.M1, motor.Dir.CW, 50)
+        motor.motor_run(motor.Motors.M4, motor.Dir.CW, 50)
     
 forever(on_forever)
 
--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
 let light_sensor = 0
-Math.random()
 forever(function on_forever() {
     
     light_sensor = pins.analogReadPin(AnalogPin.P1)
-    if (light_sensor >= 700) {
+    if (light_sensor >= 850) {
         pins.digitalWritePin(DigitalPin.P16, 1)
+        // back up
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 75)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 85)
+        basic.pause(1500)
+        // spin around at a random degree
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 75)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 75)
+        basic.pause(randint(750, 2000))
     } else {
+        // we're on white, drive forward
         pins.digitalWritePin(DigitalPin.P16, 0)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 50)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 50)
     }
     
 })
